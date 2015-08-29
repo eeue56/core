e5601d82fa35fdc120ab35b231d3119a87f8db54
eeue56 <eeue56@users.noreply.github.com>
2015-08-30 00:18:36 +0100

    update regex tests to account for result

--- tests/Test/Regex.elm
+++ tests/Test/Regex.elm
@@ -11,26 +11,29 @@ import ElmTest.Test exposing (..)
 tests : Test
 tests =
   let simpleTests = suite "Simple Stuff"
-        [ test "split All" <| assertEqual ["a", "b"] (split All (regex ",") "a,b")
-        , test "split" <| assertEqual ["a","b,c"] (split (AtMost 1) (regex ",") "a,b,c")
+        [ test "split All" <| assertEqual ["a", "b"] (split All (comma) "a,b")
+        , test "split" <| assertEqual ["a","b,c"] (split (AtMost 1) (comma) "a,b,c")
         , test "find All" <| assertEqual
             ([Match "a" [] 0 1, Match "b" [] 1 2])
-            (find All (regex ".") "ab")
+            (find All (case regex "." of Result v -> v) "ab")
         , test "find All" <| assertEqual
             ([Match "" [] 0 1])
-            (find All (regex ".*") "")
+            (find All (case regex ".*" of Result v -> v) "")
 
         , test "replace AtMost 0" <| assertEqual             "The quick brown fox"
-            (replace (AtMost 0) (regex "[aeiou]") (\_ -> "") "The quick brown fox")
+            (replace (AtMost 0) (vowels) (\_ -> "") "The quick brown fox")
 
         , test "replace AtMost 1" <| assertEqual             "Th quick brown fox"
-            (replace (AtMost 1) (regex "[aeiou]") (\_ -> "") "The quick brown fox")
+            (replace (AtMost 1) (vowels) (\_ -> "") "The quick brown fox")
 
         , test "replace AtMost 2" <| assertEqual             "Th qick brown fox"
-            (replace (AtMost 2) (regex "[aeiou]") (\_ -> "") "The quick brown fox")
+            (replace (AtMost 2) (vowels) (\_ -> "") "The quick brown fox")
 
         , test "replace All" <| assertEqual           "Th qck brwn fx"
-            (replace All (regex "[aeiou]") (\_ -> "") "The quick brown fox")
+            (replace All (vowels) (\_ -> "") "The quick brown fox")
         ]
+      
+      vowels = case regex "[aeiou]" of Result x -> x
+      comma = case regex "," of Result x -> x
   in
       suite "Regex" [ simpleTests ]
